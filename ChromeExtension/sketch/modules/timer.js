75ac4f911fdf000c63f2b39a79b83317cd610662
csgradle <raineoden@gmail.com>
2021-03-28 08:16:46 -0700

    Update timer.js

--- ChromeExtension/sketch/modules/timer.js
+++ ChromeExtension/sketch/modules/timer.js
@@ -1,10 +1,18 @@
-import { timeTxt, stopAndResetTimer, setTimer } from '../../background.js';
+import { timeTxt, stopAndResetTimer, setTimer, pauseTimer, isPaused } from '../../background.js';
 let stopAndResetBtn = document.getElementById('stopAndResetBtn');
 let setBtn = document.getElementById('setBtn');
 let sTime = document.getElementById('startTime');
 stopAndResetBtn.onclick = function () {
-  stopAndResetTimer();
+  //stopAndResetTimer();
   document.getElementById('startTime').value = '';
+  if(isPaused) {
+  	stopAndResetBtn.innerHTML = "Pause";
+  	pauseTimer(false);
+  }
+  else {
+  	stopAndResetBtn.innerHTML = "Play";
+  	pauseTimer(true);
+  }
 };
 setBtn.onclick = function () {
   setTimer(sTime.value);
@@ -13,5 +21,6 @@ updateTimer();
 function updateTimer() {
   if (document.getElementById('timer'))
     document.getElementById('timer').innerHTML = timeTxt;
-  setTimeout(updateTimer, 1000);
+  
+  setTimeout(updateTimer, 100);
 }
