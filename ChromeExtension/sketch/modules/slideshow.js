0a63ca1f12ebe4468961670ecddc8ad1a3b942c7
Tuan Nguyen <tuan.nguyen@tuni.fi>
2021-03-28 17:45:00 +0700

    Update quotes slideshow

--- ChromeExtension/sketch/modules/slideshow.js
+++ ChromeExtension/sketch/modules/slideshow.js
@@ -1,12 +1,17 @@
-const quotes = [
-  'Remember to turn on your camera!',
-  "Don't be afraid to talk. I'm sure your peers would love to hear from you!",
-  'Mistakes are proof that you are trying!',
-  'Never give up! I believe in you!',
-  "Put your camera on today! You'll put a smile on your teachers face :)",
-];
+let quotes = [];
+let numberOfQuotes = 0;
 var slideIndex = 0;
-showSlides();
+
+fetch('https://type.fit/api/quotes')
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    quotes = data;
+    numberOfQuotes = quotes.length;
+  })
+  .then(showSlides);
 
 function showSlides() {
   var i;
@@ -19,9 +24,13 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = 'block';
-  let choice = Math.floor(Math.random() * 5);
+
+  let choice = Math.floor(Math.random() * numberOfQuotes);
   let quote = quotes[choice];
-  let id = `output${slideIndex}`;
-  document.getElementById(id).innerHTML = quote;
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  let quote_id = `quote${slideIndex}`;
+  let author_id = `author${slideIndex}`;
+  document.getElementById(quote_id).innerHTML = quote.text;
+  document.getElementById(author_id).innerHTML = `- ${quote.author}`;
+
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
